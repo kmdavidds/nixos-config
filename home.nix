0db4ec3e21174d38b169d405c0ad292a65348972
kmdavidds <kmdavidds@gmail.com>
2024-12-26 19:26:24 +0800

    feat: add direnv and nix-direnv

--- home.nix
+++ home.nix
@@ -12,6 +12,14 @@
     userEmail = "kmdavidds@gmail.com";
   };
 
+  programs.direnv = {
+    enable = true;
+    enableBashIntegration = true; # see note on other shells below
+    nix-direnv.enable = true;
+  };
+
+  programs.bash.enable = true;
+
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
   # introduces backwards incompatible changes.
