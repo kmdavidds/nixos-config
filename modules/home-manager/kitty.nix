36deb4ec376c62a7cda83b703edb73395d94e537
kmdavidds <kmdavidds@gmail.com>
2024-12-27 10:03:52 +0800

    fix(kitty): increase opacity

--- modules/home-manager/kitty.nix
+++ modules/home-manager/kitty.nix
@@ -12,9 +12,9 @@
     settings = {
       confirm_os_window_close = 0;
       dynamic_background_opacity = true;
-      mouse_hide_wait = "-1.0";
+      mouse_hide_wait = "1.0";
       window_padding_width = 10;
-      background_opacity = "0.5";
+      background_opacity = "0.75";
       background_blur = 5;
     };
   };
