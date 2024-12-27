a09e47585be8622158ff8c3d36b523371d5e5fea
kmdavidds <kmdavidds@gmail.com>
2024-12-27 21:45:24 +0800

    feat(git): add signing key

--- modules/nixos/git.nix
+++ modules/nixos/git.nix
@@ -3,7 +3,18 @@
 {
   programs.git = {
     enable = true;
+    lfs.enable = true;
     userName = "kmdavidds";
     userEmail = "kmdavidds@gmail.com";
+    ignores = ["*.env" "*.env.json" "*.env.yaml" "*.env.toml" "*.envrc"];
+    extraConfig = {
+      branch.sort = "committerdate";
+      commit.gpgSign = true;
+      commit.verbose = true;
+      gpg.format = "ssh";
+      init.defaultBranch = "main";
+      merge.conflictStyle = "zdiff3";
+      user.signingkey = "~/.ssh/id_ed25519.pub";
+    };
   };
 }
