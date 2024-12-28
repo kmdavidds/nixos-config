cd80a91ee3110887f3cc85ead351d29a43cf6e53
kmdavidds <kmdavidds@gmail.com>
2024-12-29 01:15:20 +0800

    fix(mkdir-nix): finalize package

--- pkgs/mkdir-nix.nix
+++ pkgs/mkdir-nix.nix
@@ -1,33 +1,5 @@
 { pkgs }:
-let
-  flakeText = ''
-    {
-      description = "A very basic nix mkShell flake";
 
-      inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-      };
-
-      outputs =
-        {
-          self,
-          nixpkgs,
-        }:
-        let
-          pkgs = nixpkgs.legacyPackages."x86_64-linux";
-        in
-        {
-          devShells."x86_64-linux".default = pkgs.mkShell {
-
-            packages = [
-              pkgs.gnumake
-            ];
-
-          };
-        };
-    }
-  '';
-in
 pkgs.writeShellApplication {
   name = "mkdir-nix";
 
@@ -37,9 +9,14 @@ pkgs.writeShellApplication {
   ];
 
   text = ''
-    echo ${flakeText} >> flake.nix
+    mkdir "$1"
+    cd "$1"
+    echo -e "{\n  description = \"A very basic nix mkShell flake\";\n\n  inputs = {\n    nixpkgs.url = \"github:nixos/nixpkgs?ref=nixos-unstable\";\n  };\n\n  outputs =\n    {\n      self,\n      nixpkgs,\n    }:\n    let\n      pkgs = nixpkgs.legacyPackages.\"x86_64-linux\";\n    in\n    {\n      devShells.\"x86_64-linux\".default = pkgs.mkShell {\n\n        packages = [\n          pkgs.gnumake\n        ];\n\n      };\n    };\n}" >> flake.nix
     echo "use flake" >> .envrc
-    git init
+    echo -e ".envrc \n.direnv" >> .gitignore
     direnv allow
+    git init
+    git add .
+    git commit -m "init flake"
   '';
 }
