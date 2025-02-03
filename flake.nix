7e1d92171272655a217c7c844ed9af92ed9b4b57
kmdavidds <kmdavidds@gmail.com>
2025-02-03 14:28:53 +0700

    fix: change stylix branch

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    stylix.url = "github:danth/stylix";
+    stylix.url = "github:danth/stylix/release-24.11";
 
     zen-browser.url = "github:0xc000022070/zen-browser-flake";
   };
