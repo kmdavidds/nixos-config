625670e3f44b4fbc8aa4a3661a3580ca407b4b32
kmdavidds <kmdavidds@gmail.com>
2025-06-26 04:51:05 +0700

    fix(kitty): hotfix convert hm options to extraConfig

--- modules/home-manager/kitty/default.nix
+++ modules/home-manager/kitty/default.nix
@@ -1,6 +1,7 @@
 { lib, ... }:
 
 {
+  # TODO: fix undetected kitty config in home manager
   programs.kitty = lib.mkForce {
     enable = true;
     font = {
@@ -20,6 +21,14 @@
     };
 
     extraConfig = ''
+      font_size 18
+      confirm_os_window_close 0
+      dynamic_background_opacity yes
+      mouse_hide_wait 1.0
+      window_padding_width 10
+      background_opacity 0.75
+      background_blur 5
+      background #1E1E1E
       map f1 launch --cwd=current --type=tab
       map Alt+c kitten kitty_grab/grab.py
     '';
