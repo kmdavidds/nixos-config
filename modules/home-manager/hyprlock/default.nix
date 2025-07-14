923da5c2c64c028cf3682e003b9d9a70dfe1c613
kmdavidds <kmdavidds@gmail.com>
2025-07-14 17:38:37 +0700

    feat: add lock screen

--- modules/home-manager/hyprlock/default.nix
+++ modules/home-manager/hyprlock/default.nix
@@ -1,34 +1,49 @@
 {
   programs.hyprlock.enable = true;
-  programs.hyprlock.settings = {
-    general = {
-      disable_loading_bar = true;
-      grace = 300;
-      hide_cursor = true;
-      no_fade_in = false;
-    };
+  programs.hyprlock.extraConfig = ''
+        animations {
+        enabled = true
+        bezier = linear, 1, 1, 0, 0
+        animation = fadeIn, 1, 5, linear
+        animation = fadeOut, 1, 5, linear
+        animation = inputFieldDots, 1, 2, linear
+    }
 
-    background = [
-      {
-        path = "/home/kmdavidds/Pictures/eveningsky.png";
-        blur_passes = 3;
-        blur_size = 8;
-      }
-    ];
+    background {
+        monitor =
+        path = /home/kmdavidds/Pictures/eveningsky.png
+        blur_passes = 3
+    }
 
-    input-field = [
-      {
-        size = "200, 50";
-        position = "0, -80";
-        monitor = "";
-        dots_center = true;
-        fade_on_empty = false;
-        font_color = "rgb(202, 211, 245)";
-        inner_color = "rgb(91, 96, 120)";
-        outer_color = "rgb(24, 25, 38)";
-        outline_thickness = 5;
-        shadow_passes = 2;
-      }
-    ];
-  };
+    input-field {
+        monitor =
+        size = 20%, 5%
+        outline_thickness = 3
+        inner_color = rgba(0, 0, 0, 0.0) # no fill
+
+        outer_color = rgba(f38ba8ee) rgba(cba6f7ee) 45deg
+        check_color = rgba(f9e2afee) rgba(fab387ee) 45deg
+        fail_color = rgba(ff6633ee) rgba(ff0066ee) 45deg
+
+        font_color = rgb(143, 143, 143)
+        fade_on_empty = false
+        rounding = 15
+
+        font_family = $font
+        placeholder_text = Input password...
+        fail_text = $PAMFAIL
+
+        # uncomment to use a letter instead of a dot to indicate the typed password
+        # dots_text_format = *
+        # dots_size = 0.4
+        dots_spacing = 0.3
+
+        # uncomment to use an input indicator that does not show the password length (similar to swaylock's input indicator)
+        # hide_input = true
+
+        position = 0, -20
+        halign = center
+        valign = center
+    }
+  '';
 }
