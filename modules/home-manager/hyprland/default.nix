b0e89f12da80638d01356db7464b563c61c77ca8
kmdavidds <kmdavidds@gmail.com>
2026-02-10 16:18:07 +0700

    feat(hyprland): add more caelestia keybinds

--- modules/home-manager/hyprland/default.nix
+++ modules/home-manager/hyprland/default.nix
@@ -1,35 +1,8 @@
 {
-  pkgs,
   lib,
   ...
 }:
 
-let
-  dailyWallpaper = pkgs.writeShellScriptBin "daily-wallpaper" ''
-    WALLPAPER_DIR="/home/kmdavidds/Projects/nixos-config/modules/home-manager/swww"
-    DAY=$(date +%u)  # 1=Monday, 7=Sunday
-
-    case $DAY in
-        1) WALLPAPER="$WALLPAPER_DIR/1.png" ;;
-        2) WALLPAPER="$WALLPAPER_DIR/2.png" ;;
-        3) WALLPAPER="$WALLPAPER_DIR/3.png" ;;
-        4) WALLPAPER="$WALLPAPER_DIR/4.png" ;;
-        5) WALLPAPER="$WALLPAPER_DIR/5.png" ;;
-        6) WALLPAPER="$WALLPAPER_DIR/6.png" ;;
-        7) WALLPAPER="$WALLPAPER_DIR/7.png" ;;
-        *) WALLPAPER="$WALLPAPER_DIR/1.png" ;;
-    esac
-
-    [[ ! -f "$WALLPAPER" ]] && WALLPAPER="$WALLPAPER_DIR/1.png"
-
-    rm /home/kmdavidds/Projects/nixos-config/modules/home-manager/swww/current-wallpaper.png
-    cp $WALLPAPER /home/kmdavidds/Projects/nixos-config/modules/home-manager/swww/current-wallpaper.png
-  '';
-
-  startupScript = pkgs.pkgs.writeShellScriptBin "start" ''
-    ${pkgs.clipse}/bin/clipse -listen &
-  '';
-in
 {
   services.swww.enable = true;
   wayland.windowManager.hyprland = {
@@ -294,6 +267,8 @@ in
         "$mainMod, P, global, caelestia:session"
         "$mainMod, R, global, caelestia:launcher"
         "$mainMod, A, global, caelestia:showall"
+        "$mainMod, N, exec, caelestia shell drawers toggle sidebar"
+        "$mainMod, I, exec, caelestia shell drawers toggle dashboard"
         "$mainMod, V, exec, kitty --class clipse -e 'clipse'"
 
         # Move focus with mainMod + arrow keys
